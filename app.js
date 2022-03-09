27ab28669a479d0150a7c6ae17a463cf10f34936
JDagsa <jayden.dagsa@gmail.com>
2022-03-09 15:40:22 +0800

    Add darkMode toggle

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // grab everything we need
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
+const darkToggle = document.querySelector("button.dark-toggle")
+const html = document.querySelector(".html")
+const sun = document.querySelector(".sun")
+const moon = document.querySelector(".moon")
 
-// add event listeners
+// add event listeners for navbar mobile
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+// add event listeners for dark mode
+darkToggle.addEventListener("click", () => {
+    html.classList.toggle("dark");
+    sun.classList.toggle("hidden");
+    moon.classList.toggle("hidden");
+  });
