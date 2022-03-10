2720330ff965da638385984c38f1b6e52ce9ef63
JDagsa <jayden.dagsa@gmail.com>
2022-03-10 16:09:14 +0800

    Change dark mode toggle position, add about us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,27 @@ module.exports = {
         serif: ["EB Garamond", ...defaultTheme.fontFamily.serif],
         mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
       },
+      screens: {
+        'md': '809px',
+      },
+      backgroundImage: {
+        'cupertinopoint': "url('../images/non-vector/1280px-Cupertinopoint.jpeg')",
+      },
+      keyframes: {
+        'fade-in-down': {
+            '0%': {
+                opacity: '0',
+                transform: 'translateY(10px)'
+            },
+            '100%': {
+                opacity: '1',
+                transform: 'translateY(0)'
+            },
+          }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 1s ease-out'
+      }
     },
   },
   plugins: [],
